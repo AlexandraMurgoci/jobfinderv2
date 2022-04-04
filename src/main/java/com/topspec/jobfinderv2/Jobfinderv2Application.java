25971ec1acdf70ca35de70082c950064141c7404
SGA <sga@pop-os.localdomain>
2022-04-04 17:15:34 +0300

    initial commit

---
+++ src/main/java/com/topspec/jobfinderv2/Jobfinderv2Application.java
@@ -0,0 +1,13 @@
+package com.topspec.jobfinderv2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Jobfinderv2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Jobfinderv2Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/topspec/jobfinderv2/Jobfinderv2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.topspec.jobfinderv2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Jobfinderv2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

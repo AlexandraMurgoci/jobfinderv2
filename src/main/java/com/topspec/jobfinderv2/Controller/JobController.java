41253cac7646cfbf01186fcbda845da76ee92f79
George Dan <george_dan3@yahoo.com>
2022-04-06 19:42:15 +0300

    STJF-10 view job details

--- src/main/java/com/topspec/jobfinderv2/Controller/JobController.java
+++ src/main/java/com/topspec/jobfinderv2/Controller/JobController.java
@@ -47,6 +47,12 @@ public class JobController {
         return "jobs";
     }
 
+    @RequestMapping("/job/info/{id}")
+    public String showById(@PathVariable String id, Model model){
+        model.addAttribute("job",jobService.findById(Long.valueOf(id)));
+        return "jobinfo";
+    }
+
     @RequestMapping("/job/delete/{id}")
     public String deleteById(@PathVariable String id){
         jobService.deleteById(Long.valueOf(id));
@@ -67,5 +73,13 @@ public class JobController {
         }
         Job saveJob=jobService.save(job);
         return "redirect:/job/list";
+    }
+    //new add
+    @RequestMapping("/job/update/{id}")
+    public ModelAndView updateProdusById(@PathVariable String id,Model model){
+        ModelAndView modelAndView = new ModelAndView("jobForm");
+        Job job = jobService.findById(Long.valueOf(id));
+        modelAndView.addObject("job",job);
+        return modelAndView;
     }
 }

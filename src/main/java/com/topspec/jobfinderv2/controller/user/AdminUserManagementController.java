1692a7c86615e5f9f90b5e52767754eeea107403
SGA <sga@pop-os.localdomain>
2022-04-09 16:31:35 +0300

    update controller to match with the FE and refactor naming

--- src/main/java/com/topspec/jobfinderv2/controller/user/AdminUserManagementController.java
+++ src/main/java/com/topspec/jobfinderv2/controller/user/AdminUserManagementController.java
@@ -1,9 +1,8 @@
-package com.topspec.jobfinderv2.controller.users.operations;
+package com.topspec.jobfinderv2.controller.user;
 
+import com.topspec.jobfinderv2.model.user.User;
 import com.topspec.jobfinderv2.service.user.CreateUser;
 import com.topspec.jobfinderv2.service.user.CreateUserDto;
-import com.topspec.jobfinderv2.service.user.authentification.LoginUser;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
@@ -15,21 +14,18 @@ import static java.util.Objects.requireNonNull;
 
 @RestController
 @CrossOrigin("http://localhost:4200")
-public class UserController {
+public class AdminUserManagementController {
 
 
     private final CreateUser createUser;
 
-    public UserController(CreateUser createUser) {
+    public AdminUserManagementController(CreateUser createUser) {
         this.createUser = requireNonNull(createUser);
     }
 
-    @PostMapping("/user")
-    public ResponseEntity createUser (@RequestBody CreateUserDto createUserDto){
-        try{
-            return ResponseEntity.status(HttpStatus.OK).body(createUser.create(createUserDto));
-        }catch(Exception ex){
-            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(ex.getMessage());
-        }
+
+    @PostMapping("/admin/user/create")
+    public ResponseEntity<User> createUser (@RequestBody CreateUserDto createUserDto){
+        return ResponseEntity.status(HttpStatus.OK).body(createUser.create(createUserDto));
     }
 }
